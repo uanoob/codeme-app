13a5c23a1bc49246a7ddfe6f2509fd13aa774513
uanoob <usnet@ukr.net>
2018-10-24 21:38:57 +0300

    Add default Route -> App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect
+} from 'react-router-dom';
 import { Provider } from 'react-redux';
 import './App.css';
 import store from './store/store';
@@ -18,6 +23,7 @@ const App = () => (
           <Route exact path="/login" component={LoginUser} />
           <Route exact path="/register" component={RegisterUser} />
           <Route exact path="/profile" component={ProfileUser} />
+          <Redirect to="/" />
         </Switch>
       </Layout>
     </Router>
