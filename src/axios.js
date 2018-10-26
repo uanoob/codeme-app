9d40a85097fd55688ee9d1d1736c01101eba8bf5
uanoob <usnet@ukr.net>
2018-10-26 12:24:08 +0300

    Create axios instance

---
+++ src/axios.js
@@ -0,0 +1,10 @@
+import axios from 'axios';
+
+const axiosInstance = axios.create({
+  baseURL: 'https://incode-blog-internship.herokuapp.com',
+  headers: !localStorage.getItem('token')
+    ? {}
+    : { Authorization: `Bearer ${localStorage.getItem('token')}` },
+});
+
+export default axiosInstance;
