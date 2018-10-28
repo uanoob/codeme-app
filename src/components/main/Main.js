246b3a31ff5abaa41564c5ac8433fb79ca965c5d
uanoob <usnet@ukr.net>
2018-10-28 16:12:23 +0200

    Add grid containers in Main for -> Sidebar & Posts

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -1,10 +1,52 @@
 import React from 'react';
-import Header from '../../containers/header/Header';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+import Sidebar from '../sidebar/Sidebar';
+import Posts from '../../containers/post/Posts';
 
-export default function Main() {
+const styles = theme => ({
+  container: {
+    display: 'grid',
+    gridTemplateColumns: 'repeat(12, 1fr)',
+    gridGap: `${theme.spacing.unit * 3}px`,
+  },
+  paper: {
+    padding: theme.spacing.unit,
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+    whiteSpace: 'nowrap',
+    marginBottom: theme.spacing.unit,
+  },
+});
+
+function CSSGrid(props) {
+  const { classes } = props;
+
   return (
     <div>
-      <Header />
+      <Grid container spacing={24}>
+        <Grid item xs={4}>
+          <Paper className={classes.paper}>
+            <Sidebar />
+          </Paper>
+        </Grid>
+        <Grid item xs={8}>
+          <Paper className={classes.paper}>
+            <Posts />
+          </Paper>
+        </Grid>
+      </Grid>
     </div>
   );
 }
+
+CSSGrid.propTypes = {
+  classes: PropTypes.shape({
+    container: PropTypes.string.isRequired,
+    paper: PropTypes.string.isRequired,
+  }).isRequired,
+};
+
+export default withStyles(styles)(CSSGrid);
