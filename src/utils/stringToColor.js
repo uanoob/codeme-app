6a8b95dcd1aa6d8512c19ee9ade34c6952251f77
uanoob <usnet@ukr.net>
2018-10-30 08:14:45 +0200

    Add to utils -> stringToColor

---
+++ src/utils/stringToColor.js
@@ -0,0 +1,15 @@
+const stringToColour = function (str) {
+  let hash = 0;
+  for (var i = 0; i < str.length; i++) {
+    hash = str.charCodeAt(i) + ((hash << 5) - hash);
+  }
+  let colour = '#';
+  for (var i = 0; i < 3; i++) {
+    const value = (hash >> (i * 8)) & 0xff;
+    colour += (`00${value.toString(16)}`).substr(-2);
+  }
+  console.log(colour);
+  return colour;
+};
+
+export default stringToColour;
