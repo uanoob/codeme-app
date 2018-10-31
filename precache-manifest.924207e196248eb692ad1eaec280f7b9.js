de8fe44d77d03d42e2d6ae9522deb9700fdec532
uanoob <usnet@ukr.net>
2018-10-31 20:29:58 +0200

    Updates

---
+++ precache-manifest.924207e196248eb692ad1eaec280f7b9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1836f5bd22877a3909be",
+    "url": "/codeme-app/static/js/runtime~main.1836f5bd.js"
+  },
+  {
+    "revision": "2d06c644f9a71a4a522a",
+    "url": "/codeme-app/static/js/main.2d06c644.chunk.js"
+  },
+  {
+    "revision": "2fd784188b327b7886a4",
+    "url": "/codeme-app/static/js/1.2fd78418.chunk.js"
+  },
+  {
+    "revision": "2d06c644f9a71a4a522a",
+    "url": "/codeme-app/static/css/main.5585e831.chunk.css"
+  },
+  {
+    "revision": "4dfd6d4ff47e5df5bbc5a0c34c6ca21e",
+    "url": "/codeme-app/index.html"
+  }
+];
